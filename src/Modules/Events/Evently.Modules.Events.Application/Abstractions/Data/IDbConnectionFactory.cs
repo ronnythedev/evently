f6f30480bc2e46acc7755ccdf8b061b07873d900
Ronny Delgado <delgado.ronny@gmail.com>
2025-05-30 10:58:51 -0600

    feat: add application layer with two use cases (GetEvents, CreateEvent)

---
+++ src/Modules/Events/Evently.Modules.Events.Application/Abstractions/Data/IDbConnectionFactory.cs
@@ -0,0 +1,7 @@
+using System.Data.Common;
+namespace Evently.Modules.Events.Application.Abstractions.Data;
+
+public interface IDbConnectionFactory
+{
+    ValueTask<DbConnection> OpenConnectionAsync();
+}

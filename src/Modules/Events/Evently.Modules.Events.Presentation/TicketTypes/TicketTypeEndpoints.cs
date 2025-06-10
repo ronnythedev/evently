86ab08017f8aac949a8205b4d428a2976e9930cb
Ronny Delgado <delgado.ronny@gmail.com>
2025-06-10 11:09:35 -0600

    feat: create GetTicketType endpoint

--- src/Modules/Events/Evently.Modules.Events.Presentation/TicketTypes/TicketTypeEndpoints.cs
+++ src/Modules/Events/Evently.Modules.Events.Presentation/TicketTypes/TicketTypeEndpoints.cs
@@ -6,5 +6,6 @@ public static class TicketTypeEndpoints
     public static void MapEndpoints(IEndpointRouteBuilder app)
     {
         CreateTicketType.MapEndpoint(app);
+        GetTicketType.MapEndpoint(app);
     }
 }

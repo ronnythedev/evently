0959f0ea1dd16907e59976df9506779ad876215a
Ronny Delgado <delgado.ronny@gmail.com>
2025-06-10 10:56:49 -0600

    feat: add TicketType tag for endpoints

--- src/Modules/Events/Evently.Modules.Events.Presentation/Tags.cs
+++ src/Modules/Events/Evently.Modules.Events.Presentation/Tags.cs
@@ -4,4 +4,5 @@ internal static class Tags
 {
     internal const string Events = "Events";
     internal const string Categories = "Categories";
+    internal const string TicketTypes = "TicketTypes";
 }

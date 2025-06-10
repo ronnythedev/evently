107a1b46f708bae01f99bd29f1d5336a5609e9d6
Ronny Delgado <delgado.ronny@gmail.com>
2025-06-10 10:53:23 -0600

    feat: TicketType database set and configuration

--- src/Modules/Events/Evently.Modules.Events.Infrastructure/Database/EventsDbContext.cs
+++ src/Modules/Events/Evently.Modules.Events.Infrastructure/Database/EventsDbContext.cs
@@ -1,6 +1,8 @@
 ﻿using Evently.Modules.Events.Application.Abstractions.Data;
 using Evently.Modules.Events.Domain.Categories;
+using Evently.Modules.Events.Domain.TicketTypes;
 using Evently.Modules.Events.Infrastructure.Events;
+using Evently.Modules.Events.Infrastructure.TicketTypes;
 using Microsoft.EntityFrameworkCore;
 
 namespace Evently.Modules.Events.Infrastructure.Database;
@@ -10,11 +12,15 @@ public sealed class EventsDbContext(DbContextOptions<EventsDbContext> options) :
     internal DbSet<Domain.Events.Event> Events { get; set; }
     
     internal DbSet<Category> Categories { get; set; }
+    
+    internal DbSet<TicketType> TicketTypes { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.HasDefaultSchema(Schemas.Events);
 
         modelBuilder.ApplyConfiguration(new EventConfiguration());
+
+        modelBuilder.ApplyConfiguration(new TicketTypeConfiguration());
     }
 }

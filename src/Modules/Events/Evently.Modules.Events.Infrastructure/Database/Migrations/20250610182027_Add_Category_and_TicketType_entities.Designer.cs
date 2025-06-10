1cac20fac57a24b60fa72d2d0e3d82e5f0845299
Ronny Delgado <delgado.ronny@gmail.com>
2025-06-10 13:59:06 -0600

    feat: add new migration

--- src/Modules/Events/Evently.Modules.Events.Infrastructure/Database/Migrations/20250610182027_Add_Category_and_TicketType_entities.Designer.cs
+++ src/Modules/Events/Evently.Modules.Events.Infrastructure/Database/Migrations/20250610182027_Add_Category_and_TicketType_entities.Designer.cs
@@ -3,17 +3,20 @@ using System;
 using Evently.Modules.Events.Infrastructure.Database;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 #nullable disable
 
-namespace Evently.Modules.Events.Api.Database.Migrations
+namespace Evently.Modules.Events.Infrastructure.Database.Migrations
 {
     [DbContext(typeof(EventsDbContext))]
-    partial class EventsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250610182027_Add_Category_and_TicketType_entities")]
+    partial class Add_Category_and_TicketType_entities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

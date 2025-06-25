1d2015e0d1a13f22ec0d420d48794d8c62ee4dcd
Ronny Delgado <delgado.ronny@gmail.com>
2025-06-25 14:49:06 -0600

    feat: call new common infrastructure project from program

--- src/API/Evently.Api/Program.cs
+++ src/API/Evently.Api/Program.cs
@@ -1,5 +1,6 @@
 using Evently.Api.Extensions;
 using Evently.Common.Application;
+using Evently.Common.Infrastructure;
 using Evently.Modules.Events.Infrastructure;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
@@ -8,6 +9,8 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddApplication([Evently.Modules.Events.Application.AssemblyReference.Assembly]);
+
+builder.Services.AddInfrastructure(builder.Configuration.GetConnectionString("Database")!);
 
 builder.Services.AddEventsModule(builder.Configuration);
 

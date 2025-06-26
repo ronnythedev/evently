1fe8cb4f55373ff98b4bd0175b6694651a51b2a8
Ronny Delgado <delgado.ronny@gmail.com>
2025-06-26 16:41:58 -0600

    feat: setup serilog for structure log cross-cutting concern

--- src/Common/Evently.Common.Application/ApplicationConfiguration.cs
+++ src/Common/Evently.Common.Application/ApplicationConfiguration.cs
@@ -1,4 +1,5 @@
 ﻿using System.Reflection;
+using Evently.Common.Application.Behaviors;
 using FluentValidation;
 using Microsoft.Extensions.DependencyInjection;
 namespace Evently.Common.Application;
@@ -12,6 +13,8 @@ public static class ApplicationConfiguration
         services.AddMediatR(config =>
         {
             config.RegisterServicesFromAssemblies(moduleAssemblies);
+
+            config.AddOpenBehavior(typeof(RequestLoggingPipelineBehavior<,>));
         });
 
         services.AddValidatorsFromAssemblies(moduleAssemblies, includeInternalTypes: true);

4caba598e3a59280bf17298330287d7eb505cac9
Ronny Delgado <delgado.ronny@gmail.com>
2025-11-13 15:59:39 -0600

    feat: implement exception handling as a cross cutting concern

--- src/Common/Evently.Common.Application/ApplicationConfiguration.cs
+++ src/Common/Evently.Common.Application/ApplicationConfiguration.cs
@@ -14,6 +14,8 @@ public static class ApplicationConfiguration
         {
             config.RegisterServicesFromAssemblies(moduleAssemblies);
 
+            config.AddOpenBehavior(typeof(ExceptionHandlingPipelineBehavior<,>));
+            
             config.AddOpenBehavior(typeof(RequestLoggingPipelineBehavior<,>));
         });
 
